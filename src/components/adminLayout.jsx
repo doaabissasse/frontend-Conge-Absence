56bd1b28472f25415cc976bbcdc446ba580133a5
doaabissasse <doaa10biss@gmail.com>
2024-08-29 10:08:45 +0200

    ajouter des commentaires

--- src/components/adminLayout.jsx
+++ src/components/adminLayout.jsx
@@ -4,37 +4,30 @@ import AuthService from '../tests/AuthService';
 import '../componentsSCC/adminLayout.css';
 import { FaUsers, FaCalendarAlt } from 'react-icons/fa';
 
-/**
- * Layout principal pour les administrateurs
- * Affiche la barre supérieure, la barre latérale et le contenu principal
- */
 const AdminLayout = ({ children }) => {
   const navigate = useNavigate();
   const [user, setUser] = useState(null);
 
-  // Récupère les détails de l'utilisateur lors du chargement du composant
   useEffect(() => {
     const fetchUserDetails = async () => {
       try {
         const response = await AuthService.getUserDetails();
         setUser(response.data);
       } catch (error) {
-        console.error('Failed to fetch user details:', error); // Affiche une erreur en cas d'échec
+        console.error('Failed to fetch user details:', error);
       }
     };
 
     fetchUserDetails();
   }, []);
 
-  // Déconnexion de l'utilisateur
   const handleSignout = async () => {
     await AuthService.signout();
-    navigate('/login', { replace: true }); // Redirige vers la page de connexion après déconnexion
+    navigate('/login', { replace: true });
   };
 
   return (
     <div className="container-fluid p-0">
-      {/* Barre supérieure */}
       <div className="row top-bar-custom py-2 px-4 align-items-center">
         <div className="col-md-1">
           <img src="/lofowhite.png" alt="Logo" className="topbar-logo" />
@@ -42,18 +35,17 @@ const AdminLayout = ({ children }) => {
         <div className="col-md-9 name">
           {user && (
             <h6>
-              Welcome, {user.nom} {user.prenom} {/* Affiche le nom de l'utilisateur */}
+              Welcome, {user.nom} {user.prenom}
             </h6>
           )}
         </div>
         <div className="col-md-2 text-right">
           <button className="btn" onClick={handleSignout}>
-            Signout {/* Bouton de déconnexion */}
+            Signout
           </button>
         </div>
       </div>
 
-      {/* Barre latérale et contenu principal */}
       <div className="row">
         <nav id="sidebar" className="col-md-3 col-lg-2 d-md-block bg-light sidebar">
           <div className="position-sticky">
@@ -71,26 +63,33 @@ const AdminLayout = ({ children }) => {
                   <FaCalendarAlt size={20} />&nbsp;&nbsp;&nbsp;Suivre Absences
                 </Link>
               </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/EmployeeList">
-                  <FaUsers size={20} />&nbsp;&nbsp;&nbsp;Employés
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/leave-requestALL">
-                  <i className="fas fa-check"></i>&nbsp;&nbsp;&nbsp;Demandes Traitées
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/AbsencesListAdmin">
-                  <i className="fas fa-user-minus"></i>&nbsp;&nbsp;&nbsp;Les absences
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/AbsenceFormAdmin">
-                  <i className="fas fa-pen"></i>&nbsp;&nbsp;&nbsp;Enregistrer une Absence
-                </Link>
-              </li>
+
+              {/* Boutons visibles uniquement pour les administrateurs */}
+              {user && user.role === 'ADMIN' && (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/EmployeeList">
+                      <FaUsers size={20} />&nbsp;&nbsp;&nbsp;Employés
+                    </Link>
+                 </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/leave-requestALL">
+                      <i className="fas fa-check"></i>&nbsp;&nbsp;&nbsp;Demandes Traitées
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/AbsencesListAdmin">
+                      <i className="fas fa-user-minus"></i>&nbsp;&nbsp;&nbsp;Les absences
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/AbsenceFormAdmin">
+                      <i className="fas fa-pen"></i>&nbsp;&nbsp;&nbsp;Enregistrer une Absence
+                    </Link>
+                  </li>
+                </>
+              )}
+
               <li className="nav-item">
                 <Link className="nav-link" to="/PendingLeaveRequests">
                   <i className="fas fa-clock"></i>&nbsp;&nbsp;&nbsp;Demandes En attente
@@ -106,7 +105,7 @@ const AdminLayout = ({ children }) => {
         </nav>
 
         <main role="main" className="col-md-9 ms-sm-auto col-lg-10 px-4">
-          {children} {/* Contenu principal rendu ici */}
+          {children}
         </main>
       </div>
     </div>
